27b98076f1a488211f4d6b1286d649db73d25134
Mohan Kumar <mohanrathore98@gmail.com>
2021-01-02 22:01:51 +0530

    Javascript concept

---
+++ JavaScript/math.js
@@ -0,0 +1,22 @@
+
+
+let temp = 167;
+
+console.log(Math.sqrt(temp));
+
+console.log(Math.pow(2,3));
+console.log(Math.exp(10));
+console.log(Math.max(10,20,15));
+console.log(Math.min(10,20,15));
+console.log(Math.floor(Math.random()*10));
+
+//Number
+
+let n = new Number(20.6);
+
+//Boolean
+
+let bool = new Boolean(true);
+let bool1 = true;
+console.log(bool);
+console.log(bool1);
