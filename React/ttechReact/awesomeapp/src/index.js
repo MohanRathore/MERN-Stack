520ef3aff012b019c4cc59402f13ea1ef5b5d3b8
Mohan Kumar <mohanrathore98@gmail.com>
2021-01-03 12:38:31 +0530

    React concept

---
+++ React/ttechReact/awesomeapp/src/index.js
@@ -0,0 +1,10 @@
+//var React = require("react");
+import React from "react";
+import ReactDOM from "react-dom";
+//var ReactDOM = require("react-dom");
+
+// ReactDOM.render("kya dikhana hai","kaha dikhana hai","callback")
+
+ReactDOM.render(<h1>Hello Mohan Welcome!!</h1>,document.getElementById("root"));
+
+//we import react module to use JSX - JAvascript XML or extension
