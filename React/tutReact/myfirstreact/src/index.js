1b34c4586f3f61ad1fcea8a09c8d9677a83dac61
Mohan Kumar <mohanrathore98@gmail.com>
2021-01-04 19:33:46 +0530

    React concept

--- React/tutReact/myfirstreact/src/index.js
+++ React/tutReact/myfirstreact/src/index.js
@@ -1,4 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello Mohan Welcome</h1>,document.getElementById("root"));
+const myelement = (
+    <table>
+        <tr>
+            <th>Name</th>
+        </tr>
+        <tr>
+            <td>John</td>
+        </tr>
+        <tr>
+            <td>Elsa</td>
+        </tr>
+    </table>
+);
+
+ReactDOM.render(<h1>Hello Mohan Welcome and react is {5+5} times better</h1>,document.getElementById("root"));
+// ReactDOM.render(<h1>Hello Mohan Welcome and react is {5+5} times better</h1>,document.getElementById("root"));
+// ReactDOM.render(myelement,document.getElementById("root"));
