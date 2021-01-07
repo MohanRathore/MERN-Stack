8b5d2101291712005879f1ce6635c776c267d8c3
Mohan Kumar <mohanrathore98@gmail.com>
2021-01-07 21:35:07 +0530

    JavaScript Concept

---
+++ JavaScript/objectPrototype/fetchapi.js
@@ -0,0 +1,10 @@
+let responce = fetch("url");
+
+
+fetch(url)
+    .then((responce) =>{
+        //handle the responce;
+    })
+    .catch((error)=>{
+        //handles the error;
+    });
---
+++ JavaScript/objectPrototype/tryctach.js
@@ -0,0 +1,5 @@
+try {
+    mohan();
+} catch (error) {
+    console.log(error.name + ":" + error.message);
+}
