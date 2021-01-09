5adb6f2fdd88adaf5b726eef2c69b0f752ab4248
Mohan Kumar <mohanrathore98@gmail.com>
2021-01-09 22:53:46 +0530

    JavaScript Concept

---
+++ JavaScript/JSDOM/selectingElement.js
@@ -0,0 +1,14 @@
+//getting element by id
+let element = document.getElementById(root);
+console.log(element);
+
+//getting element by name
+let element1 = document.getElementsByName(rate);
+
+//getting element by tag name
+let element2 = document.getElementsByTagName(h2);
+
+//getting element by class name
+let element3 = document.getElementsByClassName(root);   
+
+//quary selector -> selecting element by css selector
