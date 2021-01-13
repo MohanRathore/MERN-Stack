f0c1310f57eb4bcf734ae06a478f509b2ac9f163
Mohan Kumar <mohanrathore98@gmail.com>
2021-01-13 23:51:33 +0530

    JavaScript Concept

--- JavaScript/practice/datetime.js
+++ JavaScript/practice/datetime.js
@@ -11,3 +11,23 @@ var datetime = "Last Sync: " + currentdate.getDate() + "/"
                 + currentdate.getHours() + ":"  
                 + currentdate.getMinutes() + ":" 
                 + currentdate.getSeconds();
+
+                <script>
+//function for displaying values
+function dis(val)
+{
+document.getElementById("edu").value+=val
+ }
+//function for evaluation
+function solve()
+{
+let x = document.getElementById("edu").value
+let y = eval(x)
+document.getElementById("edu").value = y
+}
+//function for clearing the display
+function clr()
+{
+document.getElementById("edu").value = ""
+}
+</script>
