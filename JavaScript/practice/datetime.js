0c5b310006ec62df694def50b7c3e04140f4c635
Mohan Kumar <mohanrathore98@gmail.com>
2021-01-13 23:47:25 +0530

    JavaScript Concept

--- JavaScript/practice/datetime.js
+++ JavaScript/practice/datetime.js
@@ -3,3 +3,11 @@ let date = new Date();
 console.log(date.getDay());
 console.log(date.getTime());
 console.log(date.getMonth());
+
+var currentdate = new Date(); 
+var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
